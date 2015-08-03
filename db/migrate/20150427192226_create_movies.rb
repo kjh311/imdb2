fa0b1c87e52b5845ec0a09dbd0427bb222273d46
Kevin Huelsmann <kjh311@gmail.com>
2015-08-03 15:17:43 -0700

    Banaynas

--- db/migrate/20150427192226_create_movies.rb
+++ db/migrate/20150427192226_create_movies.rb
@@ -2,9 +2,10 @@ class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
       t.text :title
-      t.text :summary
       t.text :youtube_embed_id
+      t.text :summary
       t.text :thumbnail
+      t.integer :actors
 
       t.timestamps
     end
