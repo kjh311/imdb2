a103dc89e98d95b8321c4309e0501b2ebdeffd69
Kevin Huelsmann <kjh311@gmail.com>
2015-08-03 16:56:15 -0700

    new movie works but not video

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,18 +6,32 @@ class MoviesController < ApplicationController
 
   def new
     @movie = Movie.new
-
   end
 
+  def create
+  @movie = Movie.new(movie_params)
+  if @movie.save
+   redirect_to movies_path
+  else
+   render 'new'
+  end
+ end
+
   def edit
+
 
   end
 
   def  show
-
+    @movie = Movie.find(params[:id])
   end
 
+  private
 
-
-
+  def movie_params
+    params.require(:movie).permit(:title, :summary, :youtube_embed_id, :thumbnail)
+  end
 end
+
+
+
