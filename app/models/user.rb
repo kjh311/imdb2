afba14b633f07a18baa7b06233b092e098c0d42a
Kevin Huelsmann <kjh311@gmail.com>
2015-08-03 18:42:40 -0700

    finished?

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
   has_many :reviews
   has_many :movies, through: :reviews
 
-  full_name = "first_name " + "last_name"
+  def full_name
+    "#{first_name }  #{last_name}"
+  end
 end
